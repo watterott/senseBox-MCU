7b2f537e3cf1ed723cc165767914f68428c773ab
Andreas Watterott <1488433+awatterott@users.noreply.github.com>
2018-02-01 20:33:41 +0100

    added microSD example

---
+++ arduino/samd/libraries/SenseBox/examples/Test_microSD/Test_microSD.ino
@@ -0,0 +1,66 @@
+/*
+  Test microSD 
+
+  Test progam for microSD card (mSD-Bee), connected to XBEE2 (SPI).
+*/
+
+#include <SPI.h>
+#include <SD.h>
+#include <SenseBoxIO.h>
+
+// set up variables using the SD utility library functions:
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+
+const int chipSelect = SDCARD_SS_PIN; // 24 is CS XB1, 28 is CS XB2
+
+void setup()
+{
+  // init serial library
+  Serial.begin(9600);
+  while(!Serial); // wait for serial monitor
+  Serial.println("Test microSD");
+
+  // microSD in XBEE2 Socket
+  senseBoxIO.PowerXB2(true);
+
+  // init card
+  if(!card.init(SPI_HALF_SPEED, chipSelect))
+  {
+    Serial.println("Error - Not Found");
+    senseBoxIO.StatusRed();
+    return; // don't continue
+  }
+
+  // print card type
+  Serial.print("\nCard Type: ");
+  switch(card.type())
+  {
+    case SD_CARD_TYPE_SD1:  Serial.println("SD1");     break;
+    case SD_CARD_TYPE_SD2:  Serial.println("SD2");     break;
+    case SD_CARD_TYPE_SDHC: Serial.println("SDHC");    break;
+    default:                Serial.println("Unknown"); break;
+  }
+
+  // init volume/partition
+  if(!volume.init(card))
+  {
+    Serial.println("Error - Not Found FAT16/FAT32 Partition");
+    senseBoxIO.StatusRed();
+    return; // don't continue
+  }
+
+  // open/list volume/partition
+  Serial.println("\nFiles:");
+  root.openRoot(volume);
+  root.ls(LS_R | LS_DATE | LS_SIZE);
+
+  // status green
+  senseBoxIO.StatusGreen();
+}
+
+void loop()
+{
+  // do nothing
+}
